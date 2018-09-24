745abd5100f7befde0677206697555148290a0c8
Martin <martbelmoaw@gmail.com>
2018-09-24 20:38:23 +0300

    refactored

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 module.exports = function solveEquation(equation) {
-  let arr = equation.split(' ');
+  const arr = equation.split(' '),
+        a = arr[0],
+        b = parseInt(arr[3] + arr[4]),
+        c = parseInt(arr[7] + arr[8]);
 
-  let a = arr[0];
-  let b = parseInt(arr[3] + arr[4]);
-  let c = parseInt(arr[7] + arr[8]);
+  const discreminant = (b ** 2) - (4 * a * c),
+        answer_1 = Math.round((-b + Math.sqrt(discreminant)) / (2 * a)),
+        answer_2 = Math.round((-b - Math.sqrt(discreminant)) / (2 * a));
 
-  let discreminant = (b * b) - (4 * a * c);
-
-  let answer_1 = Math.round(((b - 2 * b) + Math.sqrt(discreminant)) / (2 * a));
-  let answer_2 = Math.round(((b - 2 * b) - Math.sqrt(discreminant)) / (2 * a));
-  let result = [answer_1, answer_2];
-
-  result.sort((a, b) => a - b);
-  return result;
+  return [answer_1, answer_2].sort((a, b) => a - b);
 }
